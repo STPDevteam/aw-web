cde047109efbd3eeacaf4755f80ae85ec9890a17
Tony <roy@awenetwork.ai>
2025-03-13 14:43:52 +0800

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,36 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
-export default defineConfig({
-  base: '/ai-town',
-  plugins: [react()],
-  server: {
-    allowedHosts: ['localhost', '127.0.0.1'],
-  },
-  optimizeDeps: {
-    include: ['@chakra-ui/react'],
-  },
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+
+
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      hmr: mode === 'development',
+      allowedHosts: ['localhost', '127.0.0.1'],
     },
-  },
-  
+
+    // define: mode === 'development'
+    //   ? {
+    //       __HMR_CONFIG_NAME__: JSON.stringify("dev-hmr-config"),
+    //     }
+    //   : {},
+    
+      base: '/ai-town',
+      plugins: [react()],
+     
+      optimizeDeps: {
+        include: ['@chakra-ui/react'],
+      },
+      resolve: {
+        dedupe: ['react', 'react-dom'],
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        },
+      },
+
+  };
 });
+
+
 
