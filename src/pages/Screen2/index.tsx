b3c017aaba82227e64b23c5cd90a5dd7cb8cbc84
Tony <roy@awenetwork.ai>
2025-03-25 08:58:44 +0800

    -

--- src/pages/Screen2/index.tsx
+++ src/pages/Screen2/index.tsx
@@ -35,66 +35,69 @@ export const Screen2:FC<{ feAgentsInfo:any, currentIndex: number }> = ({  feAgen
         <Box 
           className='w100 fx-row ai-ct h100 '  
           display={['none','none','none','flex','flex']}
-        
         > 
           <Box minW="115px" w="160px" h="100px"/>
-            <Box className='w100 fx-col center h100' >
-              <Nav/>
-              <Box className='h100'>
-                <Game feAgentsInfo={feAgentsInfo}/>
-              </Box>
+          
+
+          <Box className=' w100 fx-col ai-ct ' >
+            <Nav/>
+            <Box className='h100 '>
+              <Game feAgentsInfo={feAgentsInfo}/>
             </Box>
-        </Box>
+          </Box>
+
+       
         
         {/* mobile side */}
-        <Box 
-          w="100%" 
-          display={['flex','flex','flex','none','none']} 
-          className='fx-col ai-ct'
-          
-          >
-          <Box className='w100 center' h="105px">
-            <Text className='gray fz24'>AI Town</Text>
-          </Box>
-          <Box
-            bgImage={MapContainer}
-            bgSize="cover"
-            bgPosition='center'
-            bgRepeat="no-repeat"   
-            w="347px"
-            h="519px"
+          <Box 
+            w="100%" 
+            display={['flex','flex','flex','none','none']} 
             className='fx-col ai-ct'
-            pt="16px"
-          > 
-          <Box
-            bgImage={MapMobile}
-            w="316px"
-            h="433.5px"
-            bgSize="cover"
-            bgPosition='center'
-            bgRepeat="no-repeat"   
-            className='center'
-          >
-            <Box className='center' w="266px" h="100px" borderRadius="14px" bgColor='rgba(34, 58, 54, 0.20)' backdropFilter="blur(15px)">
-              <Text className='gray fz14'>Please access AI Town on PC</Text>
+            
+            >
+            <Box className='w100 center' h="105px">
+              <Text className='gray fz24'>AI Town</Text>
+            </Box>
+            <Box
+              bgImage={MapContainer}
+              bgSize="cover"
+              bgPosition='center'
+              bgRepeat="no-repeat"   
+              w="347px"
+              h="519px"
+              className='fx-col ai-ct'
+              pt="16px"
+            > 
+            <Box
+              bgImage={MapMobile}
+              w="316px"
+              h="433.5px"
+              bgSize="cover"
+              bgPosition='center'
+              bgRepeat="no-repeat"   
+              className='center'
+            >
+              <Box className='center' w="266px" h="100px" borderRadius="14px" bgColor='rgba(34, 58, 54, 0.20)' backdropFilter="blur(15px)">
+                <Text className='gray fz14'>Please access AI Town on PC</Text>
+              </Box>
+            </Box>
             </Box>
           </Box>
-          </Box>
-        </Box>
 
-        <Notification
-          visible={open}
-          onClose={() => {
-            dispatch(alertInfoAction({
-              open: false,
-              title: '',
-              content: ''
-            }))
-            closeModal && closeModal()
-          }}
-          title={title}
-          content={content}
-      />
+          <Notification
+            visible={open}
+            onClose={() => {
+              dispatch(alertInfoAction({
+                open: false,
+                title: '',
+                content: ''
+              }))
+              closeModal && closeModal()
+            }}
+            title={title}
+            content={content}
+          />
+       </Box>
   
     </ProgressiveBackground>
         
