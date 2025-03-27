60f388c3cf8223c51507fb24ec1df36deacf3718
Tony <roy@awenetwork.ai>
2025-03-27 10:30:31 +0800

    fix: no error is thrown when player id is repeated

--- convex/util/object.ts
+++ convex/util/object.ts
@@ -3,17 +3,19 @@ export function parseMap<Id, Serialized, Parsed>(
   constructor: new (r: Serialized) => Parsed,
   getId: (r: Parsed) => Id,
 ): Map<Id, Parsed> {
-  const out = new Map();
+  const out = new Map<Id, Parsed>();
   for (const record of records) {
     const parsed = new constructor(record);
     const id = getId(parsed);
     if (out.has(id)) {
-      throw new Error(`Duplicate ID ${id}`);
+      console.warn(`Duplicate ID found: ${id}. Skipping this record.`);
+      continue;  
     }
     out.set(id, parsed);
   }
   return out;
 }
+
 
 export function serializeMap<Serialized, T extends { serialize(): Serialized }>(
   map: Map<string, T>,
