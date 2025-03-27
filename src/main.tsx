9ad2335a534bd3ed46c2f2adae49cc4d0cf90b08
Tony <roy@awenetwork.ai>
2025-03-27 16:59:16 +0800

    feat: new contract and abi

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { ReduxProvider } from '@/redux/ReduxProvider'
-
+import { base } from 'wagmi/chains'
 
 const queryClient = new QueryClient();
 
@@ -29,7 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ReduxProvider>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider >
+          <RainbowKitProvider initialChain={base}>
             <ChakraProvider>
               <ConvexClientProvider>      
                   <Pages />
