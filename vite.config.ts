accca29a4e1a85cd0d1ba2ebfd6b7350127bab7e
Tony <roy@awenetwork.ai>
2025-04-08 16:42:13 +0800

    pref: vite's ws disconnection no longer reloads the page

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,24 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
 
+function myHmrPlugin() {
+  return {
+    name: 'disable-refresh-on-disconnect',
+    configureServer(server:any) {
+      server.ws.on('vite:ws:disconnect', () => {
+        console.log('WebSocket disconnect intercepted. Implement custom reconnect logic here.')
+      })
+    },
+  }
+}
+
+
 
 export default defineConfig(({ mode }) => {
   return {
     server: {
       hmr: mode === 'development',
+      overlay: false,
       allowedHosts: ['localhost', '127.0.0.1','ai-town-lb-1479478427.ap-northeast-1.elb.amazonaws.com','world.fun','www.world.fun','test.world.fun'],
     },
     // for dev
@@ -21,7 +34,10 @@ export default defineConfig(({ mode }) => {
     // define: {
     //   __HMR_CONFIG_NAME__: JSON.stringify(mode === 'development' ? "dev-hmr-config" : ""),
     // },
-    plugins: [react()],
+    plugins: [
+      react(),
+      myHmrPlugin()
+    ],
 
     optimizeDeps: {
       include: ['@chakra-ui/react'],
